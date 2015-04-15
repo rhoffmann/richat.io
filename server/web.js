f5e7f39ad34893c11dbdf9f0eaf41fb2761e067f
Richard Hoffmann <rjh.hoffmann@gmail.com>
2015-04-15 20:23:13 +0200

    added static express server and Procfile

---
+++ server/web.js
@@ -0,0 +1,16 @@
+var express = require('express');
+
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+
+app.use(express.static('public/dist'));
+
+
+var server = app.listen(port, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('richat.io listening at %s:%s', host, port);
+});
